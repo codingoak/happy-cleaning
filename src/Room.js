651554a28b1d55c9ac0b23c1b0430789b4931117
Daniel Eicher <d.eicher@mailbox.org>
2022-02-08 14:02:22 +0100

    Create header component

--- src/Room.js
+++ src/Room.js
@@ -1,9 +1,22 @@
 import "./Room.css";
 
-export default function Room({ text, isClean }) {
+export default function Room({
+  text,
+  description,
+  isDescriptionVisible,
+  isClean,
+}) {
+  const statusClassName = isClean
+    ? "Room__status Room__status--clean"
+    : "Room__status Room__status--dirty";
+
   return (
     <section className="Room">
-      {text} {isClean ? "apple" : "not"}
+      <header className="Room__header">
+        {text}
+        <div className={statusClassName}></div>
+      </header>
+      <p hidden={!isDescriptionVisible}>{description}</p>
     </section>
   );
 }
